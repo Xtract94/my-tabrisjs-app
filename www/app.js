9fefc1e3498c217cee18845aa31765a3e1d2a468
Xtract94 <paul.kessler94@web.de>
2016-08-04 13:39:07 +0200

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,12 +1,16 @@
 var page = tabris.create("Page", {
-  title: "Hello, World!",
+  title: "Cats",
   topLevel: true
 });
  
 var textView = tabris.create("TextView", {
   font: "24px",
   layoutData: {centerX: 0, top: 100},
-  text: "Hello World jhjhgjgj"
-}).appendTo(page);
+  text: "Hello World"
+})
+
+var image = tabris.create("ImageView", { image: "C:\Users\pkessler\Documents\my-tabrisjs-app\hund-step1-deco.png"})
+
+.appendTo(page);
  
 page.open();
